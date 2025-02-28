2cf808812abb5c42348982d4f6c94bfd60c28da5
Mohammed Choubi <moha.saoud22@gmail.com>
2025-02-28 17:03:35 +0000

    initait commit

---
+++ back-end/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors'); // Add this line
+const app = express();
+
+app.use(cors()); // Add this line
+
+// Serve static files from the uploads directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+const PORT=80
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
